aa924402ed50e8ec6f4e86835b8b3c464191e79e
Wellington Reis <wellington.costa.97@gmail.com>
2023-09-11 14:32:18 -0300

    template/update

--- theme.config.js
+++ theme.config.js
@@ -4,8 +4,7 @@ export default {
   footer: (
     <footer>
       <small>
-        <time>{YEAR}</time> © Your Name.
-        <a href="/feed.xml">RSS</a>
+        <time>{YEAR}</time> © Wellington Reis.
       </small>
       <style jsx>{`
         footer {
