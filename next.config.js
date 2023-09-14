e6214c5aafea859c822d16cf63d5ee3ccca5165c
Wellington Reis <wellington.costa.97@gmail.com>
2023-09-14 18:27:46 -0300

    Changes in the build

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-blog',
-  themeConfig: './theme.config.js',
+const withNextra = require("nextra")({
+  theme: "nextra-theme-blog",
+  themeConfig: "./theme.config.js",
   // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
-})
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // any configs you need
   images: {
     unoptimized: true,
-  }
-}
+  },
+};
 
-module.exports = withNextra(nextConfig)
+module.exports = withNextra(nextConfig);
